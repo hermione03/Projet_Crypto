91eafe493191f417a4ed12b0902b8f402937c004
Squatteuse pro <inconnunetflix2018@gmail.com>
2023-05-02 14:50:18 +0200

    Exo_1_ended

--- Exo_1(RSA).py
+++ Exo_1(RSA).py
@@ -5,7 +5,7 @@ from math import *
 #Partie 1
 
 
-# 1) ecrire une fonction qui verifie si un nombre est premier :
+# 1)Fonction qui verifie si un nombre est premier :
 
 def premier(n:int):
     d = 2
@@ -15,13 +15,8 @@ def premier(n:int):
         d+=1 
     return 1 #True --> n est premier
 
-# Test 1
-# test = premier(17)
-# print("True --> n est premier" if test == 1 else "False --> n N'est pas premier")
 
-
-
-# 2) Écrire une fonction Python qui permet de générer un nombre premier aléatoire supérieur à une borne passée en paramètre.
+# 2) Fonction qui permet de générer un nombre premier aléatoire supérieur à une borne passée en paramètre:
 
 def gen_preums(b:int):
     nb = random.randint(b + 1, 3 * b)
@@ -30,129 +25,51 @@ def gen_preums(b:int):
     return nb
 
 
-#Test 2
-# borne = 10
-# nombre_premier = gen_preums(borne)
-# print(nombre_premier)
+# 3) Fonction qui permet de calculer l'inverse modulaire d'un nombre passé en paramètre. 
 
+def bezout(a, b):#Petite optimisation de la fonction bezout avec "divmod()" de python qui renvoie le quotient et le reste dirrectement
+    x, y, u, v = 1, 0, 0, 1
 
-
-
-# 3) Écrire une fonction qui permet de calculer l'inverse modulaire d'un nombre passé en paramètre. 
-
-
-
-
-def bezout(a,b):
-    x = 1
-    y = 0
-    u = 0
-    v = 1
     while b != 0:
-        q = a//b 
-        r = a%b
-        m = x - u*q
-        n = y - v*q
-        a = b 
-        b = r 
-        x = u 
-        y = v 
-        u = m 
-        v = n
-    return [a,x,y] 
+        q, r = divmod(a, b)
+        m, n = x - u * q, y - v * q
+        a, b, x, y, u, v = b, r, u, v, m, n
 
+    return a, x, y
 
-def rec_bezout(a, b):
-    if b == 0:
-        return [a, 1, 0]
-    else:
-        q = a // b
-        r = a % b
-        res = rec_bezout(b, r)
-        d = res[0]
-        x = res[2]
-        y = res[1] - q * res[2]
-        return [d, x, y]
 
 def inverse_mod(a,n):
     #verifier si  a est inevrsible 
     if (bezout(a,n)[0] == 1):
-        print("inversible")
+        #print("inversible")
         return bezout(a,n)[1]
     else:
         print("Element non inversible !")
 
 
-# Test 3
-#print("\n Test Bezout :  ", bezout(5,7))
-# print()
-# test = inverse_mod(5,7)
-
-# print("l'inverse modulaire est :", test)
-
-
-
-
-
-
 #4) Générer deux nombres premiers aléatoires p et q , et stocker la partie n de la clé publique. Fixer une variable globale d contenant l'autre partie de la clé publique.
 
+global d,n,p,q
+
+###### Initialisations Aléatoires ###########
+# p = gen_preums(10)                        #  
+# q = gen_preums(10)                        #
+# print(f"p: {p}, q: {q}")                  #
+# n = p * q                                 #
+# print(f"n : {n}")                         #
+# phi_n = (p - 1) * (q - 1)                 #
+#print(f"φ(n) : {phi_n}" )                  #
+# d = random.randint(1, n)                  #
+#############################################
 
 
 
-p = gen_preums(10)
-q = gen_preums(10)
-print(p ,"," ,q)
-n = p * q
-φn = (p - 1) * (q - 1)
-
-d = random.randint(1, n)
-
-
-#global d
-# def public_key(b):
-#     p = gen_preums(b)
-#     q = gen_preums(b)
-#     print(p ,"," ,q)
-#     n = p * q
-#     φn = (p - 1) * (q - 1)
-#     d = random.randint(1, n)
-#     print("n :", n)
-#     print("d :", d)
-#     print("φ(n) :", φn)
-#     return[n, d, φn]
-    
-
-# #Test 4
-
-# print()
-print("n :", n)
-print("φ(n) :", φn)
-# public_key(10)
-# e = inverse_mod(d, public_key[2])
-# print()
-# print(e)
 
 
 
-#5) Écrire une fonction chiffrer_rsa qui prend en paramètre un entier n , et qui renvoie la valeur chiffrée à l'aide de la clé (d, n) . On utilisera la fonction de calcul d'exponentiation rapide modulaire vue en cours.
+#5) Fonction chiffrer_rsa qui prend en paramètre un entier n , et qui renvoie la valeur chiffrée à l'aide de la clé (d, n):
 
-
-
-# def expo_rapide(a, k, n):
-#     x = a%n
-#     if(k == 0):
-#         return 1 
-#     elif (k%2 == 0 ):
-#         aux = expo_rapide(x, k//2, n)
-#         res = (aux ** 2)%n
-#         return res
-#     else: 
-#         aux = expo_rapide(x, (k-1)//2, n)
-#         res = (x*(aux ** 2)) % n
-#         return res
-
-def expo_rapide(a, k, n):
+def expo_rapide(a, k, n):#Version itterative de la foncion  d'eponentiation rapide donnée sur le pdf du cours
     result = 1
     a = a % n
 
@@ -164,10 +81,35 @@ def expo_rapide(a, k, n):
 
     return result
 
-
-
 def chiffrer_rsa(m:int):
     return expo_rapide(m,d,n)
 
-def dechiffrer_rsa(cm:int):
-    return (cm,φn,d) 
+
+#6) Fonction dechiffrer_rsa qui prend en paramètre un entier m , qui calcule la partie privée de la clé puis qui déchiffre le message m à l'aide de la clé privée.
+
+def dechiffrer_rsa(mc:int):
+    e = inverse_mod(d,phi_n) #Calcul de la clé privée 
+    return expo_rapide(mc,e,n) 
+
+#7) Le message chiffré  qui correspond à la note 13 (avec d = 3, n= 33) est 19
+
+p = 11                                       
+q = 3                                      
+n = p * q                                  
+phi_n = (p - 1) * (q - 1)                  
+d = 3
+print(f"Le message chiffré  qui correspond à la note 13 est : {chiffrer_rsa(13)}")
+
+
+#8) Vériffer que la clé privée de déchiffrement est 7 
+
+print("La clé privée de déchiffrement est bien 7 !" if inverse_mod(3, 20) == 7 else "Il y a une erreur !")
+
+
+#9) Si S = 9 alors cela correspond à la note : 15
+print(f"Le message '9', correspond à la note :  {dechiffrer_rsa(9)}")
+
+#10) La clé privée de déchiffrement en supposant que la clé publique de chiffrement soit (c = 3, n = 55) est : -13
+
+n = 55 #p = 5 , q = 11 , phi_n = 40
+print(f"La clé privée de déchiffrement est : {inverse_mod(d, 40)}")
